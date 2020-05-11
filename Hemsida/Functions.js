648d1e5cac39e7528581c38ec7ed2b3b5c8737c5
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2020-05-11 22:05:40 +0200

    dwdw

---
+++ Hemsida/Functions.js
@@ -0,0 +1,3 @@
+function capitalizeFirstLetter(string) {                                    //Takes a sting with lowercase and turns the first letter into uppercase
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
