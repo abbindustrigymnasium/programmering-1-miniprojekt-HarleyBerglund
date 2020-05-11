1bc34b90dbab562529bc44d4de917fcde0a7ff36
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2020-05-11 15:28:32 +0200

    df

--- Hemsida/getData.js
+++ Hemsida/getData.js
@@ -1,46 +1,10 @@
-(function () {
-    var startingTime = new Date().getTime();
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName("head")[0].appendChild(script);
+"use strict";
 
-    // Poll for jQuery to come into existance
-    var checkReady = function (callback) {
-        if (window.jQuery) {
-            callback(jQuery);
-        } else {
-            window.setTimeout(function () {
-                checkReady(callback);
-            }, 20);
-        }
-    };
-
-    // Start polling...
-    checkReady(function ($) {
-        $(function () {
-            var endingTime = new Date().getTime();
-            var tookTime = endingTime - startingTime;
-            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
-        });
-    });
-})();
-
-
-$(function () {
-
-
-    var people = [];
-
-    $.getJSON('DB.json', function (data) {
-        $.each(data.person, function (i, f) {
-            var tblRow = "<tr>" + "<td>" + f.namn + "</td>" +
-                "<td>" + f.livsmedelsnummer + "</td>" + "<td>" + f.kolhydrater + "</td>" + "<td>" + f.fett + 
-                "</td>" + "</tr>" + f.fibrer + "</td>" + "</tr>"+ f.alkohol + "</td>" + "</tr>"
-            $(tblRow).appendTo("#userdata tbody");
-        });
+fetch("./DB.json")
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        console.log(data.namn)
 
     });
-
-});
