287d9cd72eefea2952586dcef5d86f9d9cbcfed3
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2020-05-12 11:25:35 +0200

    save

--- Data/xlsxTOjson.py
+++ Data/xlsxTOjson.py
@@ -2,24 +2,20 @@ from pyexcel_xlsx import get_data;
 import time;
 import json;
 
-data = get_data("C:\\Users\\s9harber\\Documents\\Kolhydraträknare\\programmering-1-miniprojekt-HarleyBerglund\\LivsmedelsDB.xlsx")
+data = get_data("C:\\Users\\s9harber\\Documents\\Kolhydraträknare\\programmering-1-miniprojekt-HarleyBerglund\\Data\\LivsmedelsDB.xlsx")
 sheetName = "Blad1";
 
 data_list = []
 # Iterate through each row and append in above list
 for i in range(0, len(data[sheetName])):
     data_list.append({
-        'namn' : data[sheetName][i][0],
-        'livsmedelsnummer' : data[sheetName][i][1],
-        'kolhydrater' : data[sheetName][i][2],
-        'fett' : data[sheetName][i][3],
-        'fibrer' : data[sheetName][i][4],
-        'alkohol' : data[sheetName][i][5]
+        'namn' : data[sheetName][i][0]
+        
         
     })
 data_list = {'intents': data_list} # Converting to required object
 j = json.dumps(data_list, indent=4)
 
 # Write to file
-with open('DB.json', 'w') as f:
+with open('Names.json', 'w') as f:
     f.write(j)
